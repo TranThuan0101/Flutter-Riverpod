3f98bda2af53f52aa24d93c2bfbf35802785f7f7
thuan.tran <thuan.tran@egitech.vn>
2025-05-22 16:38:56 +0700

    Add search functionality to product list screen

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -2,6 +2,12 @@ class Product {
   final String id;
   final String name;
   final double price;
+  final String imageAsset;
 
-  Product({required this.id, required this.name, required this.price});
+  Product({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.imageAsset,
+  });
 }
